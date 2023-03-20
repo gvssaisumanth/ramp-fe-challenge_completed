1766029c140396149dc68d456bb60a932166468b
gvssaisumanth <gvssaisumanth24@gmail.com>
2023-03-20 12:25:39 -0400

    re-completed

---
+++ src/components/Transactions/index.tsx
@@ -0,0 +1,37 @@
+import { useCallback } from "react"
+import { useCustomFetch } from "src/hooks/useCustomFetch"
+import { SetTransactionApprovalParams } from "src/utils/types"
+import { TransactionPane } from "./TransactionPane"
+import { SetTransactionApprovalFunction, TransactionsComponent } from "./types"
+
+export const Transactions: TransactionsComponent = ({ transactions }) => {
+  const { fetchWithCache, loading } = useCustomFetch() // changed from fetchWithoutCache to fetchWithCache
+
+  const setTransactionApproval = useCallback<SetTransactionApprovalFunction>(
+    async ({ transactionId, newValue }) => {
+      await fetchWithCache<void, SetTransactionApprovalParams>("setTransactionApproval", { // changed from fetchWithoutCache to fetchWithCache
+        transactionId,
+        value: newValue,
+      })
+    },
+    [fetchWithCache] // changed from fetchWithoutCache to fetchWithCache
+  )
+
+  if (transactions === null) {
+    return <div className="RampLoading--container">Loading...</div>
+  }
+
+
+  return (
+    <div data-testid="transaction-container">
+      {transactions.map((transaction) => (
+        <TransactionPane
+          key={transaction.id}
+          transaction={transaction}
+          loading={loading}
+          setTransactionApproval={setTransactionApproval}
+        />
+      ))}
+    </div>
+  )
+}
---
+++ src/hooks/usePaginatedTransactions.ts
@@ -0,0 +1,35 @@
+import { useCallback, useState } from "react"
+import { PaginatedRequestParams, PaginatedResponse, Transaction } from "../utils/types"
+import { PaginatedTransactionsResult } from "./types"
+import { useCustomFetch } from "./useCustomFetch"
+
+export function usePaginatedTransactions(): PaginatedTransactionsResult {
+  const { fetchWithoutCache, loading } = useCustomFetch()
+  const [paginatedTransactions, setPaginatedTransactions] = useState<PaginatedResponse<
+    Transaction[]
+  > | null>(null)
+
+  const fetchAll = useCallback(async () => {
+    const response = await fetchWithoutCache<PaginatedResponse<Transaction[]>, PaginatedRequestParams>(
+      "paginatedTransactions",
+      {
+        page: paginatedTransactions === null ? 0 : paginatedTransactions.nextPage,
+      }
+    )
+
+    setPaginatedTransactions((previousResponse) => {
+      if (response === null || previousResponse === null) {
+        return response
+      }
+      
+     let previousData:any = paginatedTransactions?.data // added this line
+      return { data: [...previousData,...response.data], nextPage: response.nextPage } // modified data from {response.data } to [...previousData,...response.data]
+    })
+  }, [fetchWithoutCache, paginatedTransactions])
+
+  const invalidateData = useCallback(() => {
+    setPaginatedTransactions(null)
+  }, [])
+
+  return { data: paginatedTransactions, loading, fetchAll, invalidateData }
+}
